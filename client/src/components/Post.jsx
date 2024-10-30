3c4507ba08f26cdf2811d4a11d8d7576257f9772
nikikiarie <nix.kiariebiz@gmail.com>
2024-10-30 08:23:13 +0300

    UI changes in the post component to make app clean look

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,63 +1,67 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { LuHeart } from "react-icons/lu";
 import { useDispatch, useSelector } from "react-redux";
-import { toggleLikeAsync } from '../redux/postSlicce';
+import { toggleLikeAsync } from "../redux/postSlicce";
 
-
-const Post = ({post}) => {
+const Post = ({ post }) => {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const handleLike = () => {
-      user?.token ? dispatch(toggleLikeAsync({ postId: post._id, userId: user._id })) : navigate('/email', { state: { fromPost: true } });
-      
-    };
-  
-  //   const isLiked = post?.likes instanceof Map && post.likes.has(user._id);
-  // const likeCount = post?.likes instanceof Map ? post.likes.size : 0;
-  // console.log(isLiked)
+  const handleLike = () => {
+    user?.token
+      ? dispatch(toggleLikeAsync({ postId: post._id, userId: user._id }))
+      : navigate("/email", { state: { fromPost: true } });
+  };
 
   const isLiked = post?.likes && post.likes[user._id];
   const likeCount = post?.likes ? Object.keys(post.likes).length : 0;
-  console.log(isLiked)
-  // const isLiked = Array.isArray(post.likes) && post.likes.includes(user._id);
-  // const likeCount = Array.isArray(post.likes) ? post.likes.length : 0;
-  // console.log(isLiked)
 
   return (
-    <div>
-      
-      <div key={post.id} className="py-8 max-w-sm rounded-lg overflow-hidden shadow-lg" >
-      <Link to={`/posts/${post._id}`}>
-            <img src={post.pic} alt="Blog 3" className="w-full h-48 object-cover"/>
-            <div className="px-6 pt-4 ">
-              <span className="text-indigo-600 font-semibold text-lg">{post.title}</span>
-              <p className="text-gray-700 text-base mt-2">
-                {post.description}
-              </p>
-              
+    <div className="py- max-w-sm rounded-lg overflow-hidden shadow-lg h-[400px] ">
+      <div className="h-[45%]">
+        <Link to={`/posts/${post._id}`}>
+          <img
+            src={post.pic}
+            alt="Blog"
+            className="w-full h-full object-cover"
+          />
+        </Link>
+      </div>
+      <div className="px-6 pt-4 flex flex-col justify-evenly h-[45%]">
+        <span className="text-indigo-600 font-semibold text-lg">
+          {post.title}
+        </span>
+        <p className="text-gray-700 text-base mt-2 line-clamp-3">
+          {post.description}
+        </p>
 
-            </div>
-            </Link>
-            <Link to={`/${post.authorId}/blogs`}><p className="text-gray-700 text-base mt-2 font-bold" onClick={() => navigate(`/${post.authorId}/blogs`)} >
-                {post.author}
-              </p>
-              </Link>
-              <div className="flex items-center mt-2 font-medium text-base">
-                <div className='ml-auto justify-center items-center px-6 flex'>
-                <LuHeart onClick={handleLike} className={`cursor-pointer ${isLiked ? "fill-current" : ""} ${isLiked ? 'text-red-500' : 'text-gray-500'}`}  />
+        <Link to={`/${post.authorId}/blogs`}>
+          <p
+            className="text-gray-700 text-base mt-2 font-bold"
+            onClick={() => navigate(`/${post.authorId}/blogs`)}
+          >
+            {post.author}
+          </p>
+        </Link>
+        
+      </div>
+      <div className=" px-6 flex items-center justify-end font-medium text-base h-[10%]">
+          <LuHeart
+            onClick={handleLike}
+            className={`cursor-pointer ${isLiked ? "fill-current" : ""} ${
+              isLiked ? "text-red-500" : "text-gray-500"
+            }`}
+          />
           <span className="ml-2 text-gray-700">
-          {likeCount} {likeCount === 1 ? 'Like' : 'Likes'}
+            {likeCount} {likeCount === 1 ? "Like" : "Likes"}
           </span>
-                </div>
-          
         </div>
-          
-          </div>
-    </div>
-  )
-}
 
-export default Post
+     
+    </div>
+  );
+};
+
+export default Post;
